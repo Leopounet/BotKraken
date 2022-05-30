9c03f7a2f0005f2a9b60bb7292869df3c741c7ab
Leopounet <decastrotheo960@gmail.com>
2022-05-31 01:40:11 +0200

    Auto-push!

--- src/BuyStrategies/RecentMinimum.py
+++ src/BuyStrategies/RecentMinimum.py
@@ -5,4 +5,15 @@ from Structures.AssetPair import AssetPair
 ###############################################################################
 
 def strategy(ap : AssetPair) -> float:
-    return ap.data.mean - ap.data.current
+    latest_min = None
+    latest_max = None
+
+    for date in ap.data.local_minimums:
+        if latest_min == None or date > latest_min:
+            latest_min = date
+
+    for date in ap.data.local_maximums:
+        if latest_max == None or date > latest_max:
+            latest_max = date
+
+    return latest_min - latest_max
