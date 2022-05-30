d78617f0da3c2c4801f1325ca7047a0df961574e
Leopounet <decastrotheo960@gmail.com>
2022-05-31 00:56:02 +0200

    Auto-push!

---
+++ src/SellStrategies/Random.py
@@ -0,0 +1,11 @@
+import random
+
+from Structures.AssetPair import AssetPair
+from Structures.Player import Player
+
+###############################################################################
+############################ STRATEGY #########################################
+###############################################################################
+
+def strategy(player : Player, ap : AssetPair) -> float:
+    return random.random() >= 0.5
