e26705f3f1dc53698ab0225ba9d2542d2f8f3be2
Leopounet <decastrotheo960@gmail.com>
2022-06-05 15:33:05 +0000

    Auto-push!

--- executeCommands.sh
+++ executeCommands.sh
@@ -16,8 +16,10 @@ while IFS= read -r line; do
 	else
 		if [[ "$line" = "START" ]]; then
 			to_write+=("[$(date +%F_%H-%M-%S)] $line")
-			python3 src/main.py &
-		else
+			cd src/
+			python3 main.py &
+			cd ..
+ 		else
 			echo "$line" >> $command_file
 			to_write+=("[$(date +%F_%H-%M-%S)] $line")
 		fi
